d8ebcde39bada4ca78a8f3d0fc8003139aaad5f2
Jordan Rosas <jordanrosas1203@gmail.com>
2019-03-04 10:49:07 -0600

    random practice done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+                List<int> numbers = new List<int> {
+                    random.Next(10),
+                    random.Next(10),
+                    random.Next(10),
+                    random.Next(10),
+                    random.Next(10),
+                };
+
+                for(int i = 0; i < numbers.Count; i++){
+                //Contains method built into C# 
+                    if(numbers.Contains(i)){
+                        Console.WriteLine($"Numbers List Contains {numbers[i]}");
+
+                    }else{
+                    Console.WriteLine($"Numbers List Does not contain {numbers[i]}"); 
+                }
+        }
+    }
+}
+}
